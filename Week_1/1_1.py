bc530718103b414d134fe175a5fdfc382db722f2
ririri0 <uriri.orange@gamil.com>
2021-05-14 02:01:45 +0900

    update

--- Week_1/1_1.py
+++ Week_1/1_1.py
@@ -1,18 +1,12 @@
 import collections
 
-def counter_return(words_list):
-    words_list_counter = []
-    for tmp in words_list:
-        counter = collections.Counter(tmp.replace( '\n' , '' ))
-        words_list_counter.append(counter)
-    return words_list_counter
-
-def anagram_arr(words_list, words_list_counter, give_word):
+def anagram_arr(words_list, give_word):
     tmp = collections.Counter(give_word)
     result = []
-    for i, comp in enumerate(words_list_counter):
-        if tmp == comp:
-            result.append(words_list[i].replace( '\n' , '' ))
+    for word in words_list:
+        word_tmp = collections.Counter(word)
+        if word_tmp == tmp:
+            result.append(word.replace( '\n' , '' ))
     if result == []:
         return 'WE CAN NOT FIND THE ANAGRAM WORD !'
     else:
@@ -22,8 +16,7 @@ give_word = str(input())
 f = open('words.txt', 'r')
 words_list = f.readlines()
 f.close()
-words_list_counter = counter_return(words_list)
 
 ##  全てのアナグラムを配列で出力
 ##  全く並べ替えてないワードも出力
-print(anagram_arr(words_list, words_list_counter, give_word))
+print(anagram_arr(words_list, give_word))
